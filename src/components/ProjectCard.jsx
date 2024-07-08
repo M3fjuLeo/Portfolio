75a572b225fc51f3927120600acfdc0006999e77
M3fjuLeo <mateuszropek@onet.pl>
2024-07-08 18:05:59 +0200

    projects added

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,10 +1,18 @@
-function ProjectCard() {
+function ProjectCard({
+  icon,
+  title,
+  description,
+  designer,
+  image,
+  link,
+  skills,
+}) {
   return (
     <div className="flex lg:gap-4 flex-col lg:flex-row bg-white rounded-xl shadow-md">
       <div className="flex-1 w-full">
-        <a href="https://gymate-ruby.vercel.app/" target="_blank" className="">
+        <a href={link} target="_blank" className="">
           <img
-            src="../images/Gymate.png"
+            src={image}
             alt="Project image"
             className="rounded-xl cursor-pointer object-cover w-full h-full"
           />
@@ -13,21 +21,20 @@ function ProjectCard() {
 
       <div className="px-2 py-12 lg:items-start lg:text-start items-center text-center flex flex-col gap-4 flex-1">
         <h1 className="font-bold text-xl flex gap-2">
-          <img src="../images/gymate-icon.png" className="size-8" alt="" />
-          Gymate
+          <img src={icon} className="" alt="" />
+          {title}
         </h1>
+        <p className="text-gray-800">{description}</p>
         <p className="text-gray-800">
-          Gymate provides a space for people to improve their physical fitness
-          and overall health through regular exercise and physical activity.
-          Will be developed in the future...
+          Created with{" "}
+          {skills.map((skill, index) => (
+            <span key={index} className="text-blue-500">
+              {skill}
+              {index < skills.length - 1 && ", "}
+            </span>
+          ))}
         </p>
-        <p className="text-gray-800">
-          Created with
-          <span className="text-blue-500"> React Vite </span>
-          and
-          <span className="text-blue-500"> Tailwind </span>
-        </p>
-        <p className="text-gray-800 text-sm">Designed by RadiusTheme</p>
+        <p className="text-gray-800 text-sm">{designer}</p>
       </div>
     </div>
   );
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,48 @@
 import ProjectCard from "../components/ProjectCard";
 import Section from "../components/Section";
 
+const projects = [
+  {
+    id: 1,
+    title: "Gymate",
+    link: "https://gymate-ruby.vercel.app/",
+    image: "../images/Gymate.png",
+    icon: "../images/gymate-icon.png",
+    description: `Gymate provides a space for people to improve their physical fitness
+          and overall health through regular exercise and physical activity.
+          Will be developed in the future...`,
+    designer: "Designed by RadiusTheme",
+    skills: ["React", "Vite", "Tailwind"],
+  },
+  {
+    id: 2,
+    title: "🍕 Fast React Pizza.Co",
+    link: "https://fast-react-pizza-co-lime.vercel.app/",
+    image: "../images/FastReactPizza-homePage.png",
+    description: `Simple application, where users can order one or more pizzas from a menu.`,
+    designer: "This website was created during Jonas Schmedtmann's course",
+    skills: ["React", "Tailwind", "React Router", "Redux"],
+  },
+];
+
 function Projects() {
   return (
     <Section id="projects" styles="bg-gray-50">
-      <div className="mb-40">
-        <ProjectCard />
+      <h1 className="text-center font-bold text-3xl mb-20">My Projects 👇</h1>
+
+      <div className="mb-20 flex flex-col gap-12">
+        {projects.map((project) => (
+          <ProjectCard
+            key={project.id}
+            image={project.image}
+            link={project.link}
+            icon={project.icon}
+            title={project.title}
+            description={project.description}
+            designer={project.designer}
+            skills={project.skills}
+          />
+        ))}
       </div>
 
       <h1 className="text-3xl font-bold text-center">
